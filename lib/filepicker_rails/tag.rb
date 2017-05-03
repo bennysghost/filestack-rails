59bb8a24bbe1bb6bf67019055a9aa322d87cba62
Benny Phanichkul <benny@ziplinenetwork.com>
2017-05-03 11:01:08 -0700

    add image_max

--- lib/filepicker_rails/tag.rb
+++ lib/filepicker_rails/tag.rb
@@ -6,7 +6,8 @@ module FilepickerRails
                                        :extensions, :container, :services,
                                        :drag_text, :drag_class, :store_path,
                                        :store_location, :store_access,
-                                       :store_container, :multiple, :crop_dim, :crop_ratio, :crop_force]
+                                       :store_container, :multiple, :crop_dim, :crop_ratio, :crop_force,
+                                       :image_max]
 
     FILEPICKER_OPTIONS_TO_CAMELIZE = [:max_size, :max_files, :open_to, :language]
 
